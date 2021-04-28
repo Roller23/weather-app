5b60f7bf966dbaeeb02854d98b66edb1004a9ccf
Wiktor Kania <roller47@wp.pl>
2021-04-28 14:22:59 +0200

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+
+const express = require('express')
+const app = express()
+const mustacheExpress = require('mustache-express')
+const { Client } = require('pg')
+
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+client.connect();
+
+app.engine('mustache', mustacheExpress());
+app.set('views', __dirname + '/public/views/')
+app.set('view engine', 'mustache');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index', {msg: 'hello'})
+});
